acc34951ed08063014b422b5008f97c163aceac4
Spac3Man2 <Spac3Man2>
2017-01-11 15:51:33 -0500

    first commit

---
+++ Laundry.rb
@@ -0,0 +1,28 @@
+def wear_clothes(item)
+  sample (item)
+  sleep(2)
+end
+
+def wash_clothes(temperature)
+  play(temperature)
+  sleep(1)
+end
+
+def dry_clothes(lpf_attack
+                attack)
+  sample(hpf_decay)
+  sleep(5)
+end
+
+2.times do
+  wear_clothes(:bd_boom)
+  wash_clothes(60)
+  dry_clothes(hpf_decay)
+end
+
+wear_clothes(
+  lpf_attack:
+  attack
+)
+wash_clothes(50)
+dry_clothes(5)
